33a9ab8d5d842957dce314f92ff147e5fa169ba4
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-02-10 13:23:48 +0500

    add fix of escape_url in sign_request

--- fixes/sign_request.sh
+++ fixes/sign_request.sh
@@ -2,4 +2,6 @@
 
 mount -o rw,remount /mnt/ro_disc
 chattr -i /usr/local/reductor/bin/reductor_worker.sh
+chattr -i /usr/local/reductor/bin/escape_url.php
 wget "https://raw.githubusercontent.com/carbonsoft/cb4reductor/master/bin/reductor_worker.sh" -O /usr/local/reductor/bin/reductor_worker.sh
+wget "https://raw.githubusercontent.com/carbonsoft/cb4reductor/master/bin/escape_url.php" -O /usr/local/reductor/bin/escape_url.php
